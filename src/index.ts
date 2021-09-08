ab166ce3e191ff367f28d4cc0b1aac4ef9cadc7a
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-09-08 16:33:48 +0200

    init project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Ściągnij wszystkie możliwe dane państw z pomocą API: https://restcountries.eu/. W dalszej części kursu będą one nazywane Tablicą Państw (TP).
+ * Ściągnięte dane zapisz w sposób, który pozwoli na ich ponowne wykorzystanie po zamknięciu i ponownym otwarciu przeglądarki,
+ * Przy starcie aplikacji sprawdź, czy dane państw istnieją w pamięci przeglądarki. 
+ * Jeśli nie, ściągnij je,
+ * Przy starcie aplikacji sprawdź ile czasu minęło od poprzedniego ściągnięcia danych państw. 
+ * Jeśli od ostatniego razu minęło co najmniej 7 dni, ściągnij i zapisz je ponownie.
+ * Stwórz metodę, która przy ponownym ściąganiu danych państw porówna populację między starym i nowym zestawem danych oraz wyświetli wszystkie nazwy państw, 
+ * których populacja uległa zmianie.
+ *
+ * Kod powinien być w pełni otypowany.
+ * Kod powinien posiadać pełen zestaw testów (Jest).
+ * Kod może posiadać komentarze.
+ */
